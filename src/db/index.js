6e01b7daff503f4126033aa76e6fa8851be9f3c8
mohit <mv386631@gmail.com>
2024-03-12 22:06:13 +0530

    first commit

--- src/db/index.js
+++ src/db/index.js
@@ -10,18 +10,6 @@ const pool = new Pool({
   port: process.env.DBPORT,
 });
 
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "Vasudev@12345",
-//   host: "62.72.31.25",
-//   port: 5432,
-//   database: "postgres",
-// });
-
-// module.exports = {
-//   query: (text, params) => pool.query(text, params),
-// };
-
 // const { Pool } = require("pg");
 // const pool = new Pool({
 //   connectionString: process.env.POSTGRES_URL + "?sslmode=require",

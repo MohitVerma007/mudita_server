ea0c8cad90b4a5b5b9877e8d11fc4c03a6a4cc49
mohit <mv386631@gmail.com>
2024-03-28 00:41:43 +0530

    add db

---
+++ db.js
@@ -0,0 +1,20 @@
+const { config } = require("dotenv");
+config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: process.env.DBUSER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: 5432,
+});
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connect to PostgreSQL successfully!");
+});
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
